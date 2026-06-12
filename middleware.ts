48ff17b1c00f7643ff29e3a75a85fa0187e6c0e7
kav3 <kav3.com@gmail.com>
2026-06-11 19:12:18 -0700

    6

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ export default async function middleware(
     req: any,
     res: any,
 ): Promise<void> {
-    console.log("hiiiii")
+    console.log(`**** ${req.url}`)
     // Inject a response header on every request
     // res.setHeader('X-Powered-By', 'nukejs')
     // Return without ending → request continues to routing
